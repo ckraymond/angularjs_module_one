1d7df78bfc84df270f4c5d7d13fdfc06c954aacf
Colin <craymond@Colins-MacBook-Pro.local>
2016-10-30 23:51:50 -0400

    Adjustments made to text color

--- js/app.js
+++ js/app.js
@@ -9,14 +9,17 @@
   function EazeeDietController($scope) {
     $scope.foods = "";
     $scope.feedback = "Welcome Dieter";
+    $scope.answerStyle = {};
 
     $scope.calculateDiet = function() {
       var split_foods = $scope.foods.split(",");
       var feed_back = "";
 
       if ($scope.foods == ""){
-        feed_back = "Please enter data first."
+        feed_back = "Please enter data first.";
+        $scope.answerStyle.style = {"color":"red"};
       } else {
+        $scope.answerStyle.style = {"color":"green"};
         if (split_foods.length <= 3){
           feed_back = "Enjoy!";
         } else {
